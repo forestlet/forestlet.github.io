89809120135011496cd30d5b5e60b8b91addbeeb
forestlet <hyf2021@outlook.com>
2022-12-28 13:05:48 +0800

    update fallback

--- src/components/project/Repository.vue
+++ src/components/project/Repository.vue
@@ -1,34 +1,34 @@
 <template>
-    <a :href=repository.html_url>
-        <div class="repository">
-            <h1>{{ repository.full_name }}</h1>
+  <a :href="repository.html_url">
+    <div class="repository">
+      <h1>{{ repository.full_name }}</h1>
 
-            <p class="description">{{ repository.description }}</p>
+      <p class="description">{{ repository.description }}</p>
 
-            <p class="info">
-                <el-tooltip effect="light" content="Star" placement="bottom">
-                    <span class="item">
-                        <i class="bi bi-star"></i>
-                        <span>{{ repository.stargazers_count }}</span>
-                    </span>
-                </el-tooltip>
+      <p class="info">
+        <el-tooltip effect="light" content="Star" placement="bottom">
+          <span class="item">
+            <i class="bi bi-star"></i>
+            <span>{{ repository.stargazers_count }}</span>
+          </span>
+        </el-tooltip>
 
-                <el-tooltip effect="light" content="Issues" placement="bottom">
-                    <span class="item">
-                        <i class="bi bi-info-circle"></i>
-                        <span>{{ repository.open_issues_count }}</span>
-                    </span>
-                </el-tooltip>
+        <el-tooltip effect="light" content="Issues" placement="bottom">
+          <span class="item">
+            <i class="bi bi-info-circle"></i>
+            <span>{{ repository.open_issues_count }}</span>
+          </span>
+        </el-tooltip>
 
-                <el-tooltip effect="light" content="Fork" placement="bottom">
-                    <span class="item">
-                        <i class="bi bi-git"></i>
-                        <span>{{ repository.forks_count }}</span>
-                    </span>
-                </el-tooltip>
-            </p>
-        </div>
-    </a>
+        <el-tooltip effect="light" content="Fork" placement="bottom">
+          <span class="item">
+            <i class="bi bi-git"></i>
+            <span>{{ repository.forks_count }}</span>
+          </span>
+        </el-tooltip>
+      </p>
+    </div>
+  </a>
 </template>
 
 <script setup>
@@ -39,28 +39,38 @@ const props = defineProps({ repository: Object })
 // console.log(props.repository)
 
 let repository = reactive({
-    html_url: `https://github.com/${props.repository.owner}/${props.repository.repo}`,
-    api: `https://api.github.com/repos/${props.repository.owner}/${props.repository.repo}`,
-    full_name: "",
-    description: "",
-    stargazers_count: 0,
-    open_issues_count: 0,
-    forks_count: 0,
-    // license: "",
+  html_url: `https://github.com/${props.repository.owner}/${props.repository.repo}`,
+  api: `https://api.github.com/repos/${props.repository.owner}/${props.repository.repo}`,
+  full_name: "",
+  description: "",
+  stargazers_count: 0,
+  open_issues_count: 0,
+  forks_count: 0,
+  // license: "",
 })
 
 const getInfo = () => {
-    axios
-        .get(repository.api)
-        .then((response) => {
-            let data = response.data
+  axios
+    .get(repository.api)
+    .then((response) => {
+      let data = response.data;
 
-            repository.full_name = data.full_name
-            repository.description = data.description
-            repository.stargazers_count = data.stargazers_count
-            repository.open_issues_count = data.open_issues_count
-            repository.forks_count = data.forks_count
-        });
+      repository.full_name = data.full_name;
+      repository.description = data.description;
+      repository.stargazers_count = data.stargazers_count;
+      repository.open_issues_count = data.open_issues_count;
+      repository.forks_count = data.forks_count;
+    })
+    .catch(function (error) {
+      console.log(error);
+
+      // fallback
+      repository.full_name = props.repository.full_name;
+      repository.description = props.repository.description;
+      repository.stargazers_count = props.repository.stargazers_count;
+      repository.open_issues_count = props.repository.open_issues_count;
+      repository.forks_count = props.repository.forks_count;
+    });
 };
 
 getInfo()
@@ -69,34 +79,34 @@ getInfo()
 
 <style lang="scss" scoped>
 .repository {
-    padding: 22px;
-    margin: 20px 0;
-    border-radius: 12px;
-    border: 2px solid var(--el-fill-color-lighter);
+  padding: 22px;
+  margin: 20px 0;
+  border-radius: 12px;
+  border: 2px solid var(--el-fill-color-lighter);
 
-    &:hover {
-        padding: 20px;
-        border: 4px solid var(--el-color-info-light-9);
+  &:hover {
+    padding: 20px;
+    border: 4px solid var(--el-color-info-light-9);
+  }
+
+  h1 {
+    font-size: 1.2rem;
+    padding: 20px 0;
+    font-weight: 900;
+  }
+
+  .description {
+    padding: 20px 0;
+  }
+
+  .info {
+    .item {
+      padding: 0 20px;
+
+      i {
+        margin-right: 6px;
+      }
     }
-
-    h1 {
-        font-size: 1.2rem;
-        padding: 20px 0;
-        font-weight: 900;
-    }
-
-    .description {
-        padding: 20px 0;
-    }
-
-    .info {
-        .item {
-            padding: 0 20px;
-
-            i {
-                margin-right: 6px;
-            }
-        }
-    }
+  }
 }
 </style>
--- src/data/repositories.js
+++ src/data/repositories.js
@@ -1,10 +1,50 @@
 export const all_repositories = [
-  { owner: "forestlet", repo: "bookdash-cn" },
-  { owner: "forestlet", repo: "everyday-album" },
-  { owner: "mytho-team", repo: "mytho-note" },
+  {
+    owner: "forestlet",
+    repo: "bookdash-cn",
+    full_name: "forestlet/bookdash-cn",
+    description: "🥰 A picture book translation project. I hope you like it.",
+    stargazers_count: "7",
+    open_issues_count: "0",
+    forks_count: "1",
+  },
+  {
+    owner: "mytho-team",
+    repo: "mytho-note",
+    full_name: "mytho-team/mytho-note",
+    description: "🏛️ A beautiful piece of note-taking software.",
+    stargazers_count: "1",
+    open_issues_count: "0",
+    forks_count: "0",
+  },
+  {
+    owner: "forestlet",
+    repo: "everyday-album",
+    full_name: "forestlet/everyday-album",
+    description: "🎵 Every day, I recommend albums to everyone.",
+    stargazers_count: "2",
+    open_issues_count: "0",
+    forks_count: "0",
+  },
 ];
 
 export const home_repositories = [
-  { owner: "forestlet", repo: "bookdash-cn" },
-  { owner: "mytho-team", repo: "mytho-note" },
+  {
+    owner: "forestlet",
+    repo: "bookdash-cn",
+    full_name: "forestlet/bookdash-cn",
+    description: "🥰 A picture book translation project. I hope you like it.",
+    stargazers_count: "7",
+    open_issues_count: "0",
+    forks_count: "1",
+  },
+  {
+    owner: "mytho-team",
+    repo: "mytho-note",
+    full_name: "mytho-team/mytho-note",
+    description: "🏛️ A beautiful piece of note-taking software.",
+    stargazers_count: "1",
+    open_issues_count: "0",
+    forks_count: "0",
+  },
 ];
